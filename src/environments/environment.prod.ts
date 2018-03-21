ee47c1a3875bf62fe023f2d537c7572f0fe646b4
Gian Felipe <gianfelipe@Gians-MacBook-Pro.local>
2018-03-21 13:20:23 +1100

    Delete config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyBdAtO6GETBm8Ibk5UmSOECxynxpwEIqdU",
-    authDomain: "firestore-4c1b4.firebaseapp.com",
-    databaseURL: "https://firestore-4c1b4.firebaseio.com",
-    projectId: "firestore-4c1b4",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
     storageBucket: "",
-    messagingSenderId: "1006406543071"
+    messagingSenderId: ""
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyBdAtO6GETBm8Ibk5UmSOECxynxpwEIqdU",
-    authDomain: "firestore-4c1b4.firebaseapp.com",
-    databaseURL: "https://firestore-4c1b4.firebaseio.com",
-    projectId: "firestore-4c1b4",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
     storageBucket: "",
-    messagingSenderId: "1006406543071"
+    messagingSenderId: ""
   }
 };
